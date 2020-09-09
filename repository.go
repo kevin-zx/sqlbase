289c10763432cf44b6a526f2be20721fca628fe6
kevin-zx <zhangxian887@gmail.com>
2020-09-09 09:44:45 +0800

    add config param

--- repository.go
+++ repository.go
@@ -5,21 +5,32 @@ import (
 	"fmt"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+	"log"
+	"os"
 	"strings"
+	"time"
 )
 
 type Storage struct {
 	DB *gorm.DB
 }
 
+var DefaultConfig = &gorm.Config{
+	Logger: logger.New(log.New(os.Stdout, "\r\n", log.LstdFlags), logger.Config{
+		SlowThreshold: 100 * time.Millisecond,
+		LogLevel:      logger.Error,
+		Colorful:      true,
+	}),
+}
+
 func NewStorage(engine string, port int, dbName string,
-	user string, host string, passWd string) (*Storage, error) {
+	user string, host string, passWd string, config *gorm.Config) (*Storage, error) {
 	var err error
 	//dsn := "user:pass@tcp(127.0.0.1:3306)/dbname?charset=utf8mb4&parseTime=True&loc=Local"
-	dsn:=fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local",
 		user, passWd, host, port, dbName)
-
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	db, err := gorm.Open(mysql.Open(dsn), config)
 	if err != nil {
 		return nil, err
 	}
